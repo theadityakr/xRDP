8507e0864af243e6eba77ee13df8980d4560c998
Summer <shivangi.verma@katalystpartners.com>
2024-09-19 04:31:47 -0700

    device fix

--- client/src/components/Devices.tsx
+++ client/src/components/Devices.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { LuMonitorCheck } from "react-icons/lu";
+// import React, { useContext } from "react";
+import { PiMonitorLight } from "react-icons/pi";
 
 import "../styles/form.css";
 
@@ -7,35 +7,39 @@ const Devices = () => {
 
 
     return (
-        <div className="flex-column active-devices-super-contanier">
+        <div className="flex-column active-devices-super-container">
         <h2>Active Devices</h2>
        <div className="device-container">
            
                 <div className="device">
-                <LuMonitorCheck size={60}/>
+                <PiMonitorLight  size={60}/>
                 <p>Device 1</p>
                 </div>
 
                 <div className="device">
-                <LuMonitorCheck size={60}/>
+                <PiMonitorLight  size={60}/>
                 <p>Device 2</p>
                 </div>
                 <div className="device">
-                <LuMonitorCheck size={60}/>
+                <PiMonitorLight  size={60}/>
                 <p>Device 3</p>
                 </div>
                 <div className="device">
-                <LuMonitorCheck size={60}/>
+                <PiMonitorLight  size={60}/>
                 <p>Device 4</p>
                 </div>
                 <div className="device">
-                <LuMonitorCheck size={60}/>
+                <PiMonitorLight  size={60}/>
                 <p>Device 5</p>
                 </div>
                 <div className="device">
-                <LuMonitorCheck size={60}/>
+                <PiMonitorLight  size={60}/>
                 <p>Device 6</p>
-                </div>           
+                </div> 
+                <div className="device">
+                <PiMonitorLight  size={60}/>
+                <p>Device 7</p>
+                </div>          
             </div>
         </div>
     );
