fec8bc944b2a8417db202d12d4355cbb07c5a3dd
Summer <shivangi.verma@katalystpartners.com>
2024-09-19 23:04:53 -0700

    sidebar fix

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -14,23 +14,6 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ onTabChange, activeTab }) => {
     const [isExpanded, setIsExpanded] = useState(true);
 
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         if (window.innerWidth < 700) {
-    //             setIsExpanded(true);
-    //         } else {
-    //             setIsExpanded(false);
-    //         }
-    //     };
-
-    //     window.addEventListener('resize', handleResize);
-    //     handleResize();
-
-    //     return () => {
-    //         window.removeEventListener('resize', handleResize);
-    //     };
-    // }, []);
-
     const handleClick = (tab: "Home" | "Devices") => {
         onTabChange(tab);
     };
@@ -44,16 +27,16 @@ const Sidebar: React.FC<SidebarProps> = ({ onTabChange, activeTab }) => {
             <div className="sidebar-container">
                 <div className="sidebar-item sidebar-tile" onClick={toggleSidebar}>
                     <IconContext.Provider value={{ className: "sidebarIcon" }}>
-                        <TbLayoutSidebarLeftExpand size={28} />
-                        <span>{isExpanded ? '' : 'Expand Sidebar'}</span>
+                        <TbLayoutSidebarLeftExpand size={28} title="Toggle Sidebar"/>
+                        {/* <span>{isExpanded ? '' : 'Expand Sidebar'}</span> */}
                     </IconContext.Provider>
                 </div>
                 <div className={`sidebar-item ${activeTab === "Home" ? 'active' : ''}`} onClick={() => handleClick("Home")}>
-                    <TbSmartHome size={27} />
+                    <TbSmartHome size={26} title="Home"/>
                     <span>Home</span>
                 </div>
                 <div className={`sidebar-item ${activeTab === "Devices" ? 'active' : ''}`} onClick={() => handleClick("Devices")}>
-                    <LuMonitorDot size={26} />
+                    <LuMonitorDot size={26} title="Devices"/>
                     <span>Devices</span>
                 </div>
             </div>
